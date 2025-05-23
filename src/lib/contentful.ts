049d62007930ed8201ec53e9271760e70c26ea36
DAFSTIN YOSHUA PRINCIPE PACHECO <dafstinyoshua.principe@ikasle.egibide.org>
2025-05-23 18:48:19 +0200

    contenful

--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -2,7 +2,7 @@ import contentful from 'contentful'
 
 export const contentfulClient = contentful.createClient({
   space: import.meta.env.CONTENTFUL_SPACE_ID, // ID del espacio Contentful
-  accessToken: import.meta.env.DEV // Si es un entorno de desarrollo, se usa el token de previsualización
+  accessToken: import.meta.env.CONTENTFUL_ACCESS_TOKEN // Si es un entorno de desarrollo, se usa el token de previsualización
     ? import.meta.env.CONTENTFUL_PREVIEW_TOKEN 
     : import.meta.env.CONTENTFUL_DELIVERY_TOKEN, // Si no es desarrollo, se usa el token de entrega
   host: import.meta.env.DEV 
