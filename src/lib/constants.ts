0c7d95b37e2ba5b7f349495f5e8cdf098f4730f7
DAFSTIN YOSHUA PRINCIPE PACHECO <dafstinyoshua.principe@ikasle.egibide.org>
2025-05-23 18:18:06 +0200

    toolbar

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -7,9 +7,9 @@ export const loaderAnimation = [
 ]
 
 export const LINKS = {
-  github: 'https://github.com/bue221',
-  linkedin: 'https://www.linkedin.com/in/bue221/',
-  mail: 'mailto:camiloplaza3@gmail.com',
+  github: 'https://github.com/yoshua6969',
+  linkedin: 'https://www.linkedin.com/in/dafstin-yoshua-principe-pacheco-2002p04p10/',
+  mail: 'mailto:principeyoshua@gmail.com',
   instagram: 'https://www.instagram.com/caj_ink/',
   medium: 'https://ladvace.medium.com/',
   discord: 'https://discordapp.com/users/163300027618295808',
@@ -50,111 +50,38 @@ export const SEARCH: Page = {
 // Study Page
 export const STUDIES = [
   {
-    title: 'Computer Science',
-    institution: 'Universidad Central',
-    link: 'https://www.ucentral.edu.co/',
-    date: '2019 - 2027',
+    title: 'Multiplatform Application Developer',
+    institution: 'Egibide',
+    link: 'https://www.egibide.org/campus-arriaga/',
+    date: '2021 - 2025',
   },
   {
     title: '...',
-    institution: 'Platzi',
-    link: 'https://platzi.com/',
-    date: '2018 - 2022',
+    institution: 'ClaseFlix',
+    link: 'https://claseflix.io/',
+    date: '2024 - 2024',
   },
   {
     title: '...',
-    institution: 'Udemy',
-    link: 'https://www.udemy.com/',
-    date: '2018 - 2022',
+    institution: 'Cisco Academy',
+    link: 'https://www.netacad.com/es/',
+    date: '2024 - 2024',
   },
   {
     title: '...',
     institution: 'Youtube',
     link: 'https://www.youtube.com/@midulive',
-    date: '2018 - 2022',
-  },
-  {
-    title: 'Tattoo Artist',
-    institution: 'Tattoo Academy',
-    link: 'https://www.instagram.com/tattoodcacademy/',
     date: '2024 - 2025',
   },
 ]
 
 export const EXPERIENCE = [
   {
-    company: 'Straico',
-    location: 'Bogotá D C, Colombia',
-    position: 'Software Engineer',
-    start: '2021',
-    link: 'https://straico.com/',
+    company: 'Ticmatic Solutions S.L.',
+    location: 'Alava, Spain',
+    position: 'Web Developer',
+    start: '2024',
+    link: 'https://www.ticmatic.es/',
     end: 'Current',
-    tasks: [
-      'Integration IA services with react and tailwind css',
-      'Development and build of DB with mongoDB',
-    ],
-  },
-  {
-    company: 'Spot2',
-    location: 'Mexico City, Mexico',
-    position: 'Software Engineer',
-    link: 'https://spot2.mx/',
-    start: '2021',
-    end: 'Current',
-    tasks: [
-      'Development of the Spot2 platform with the use of React, Redux, and Material UI',
-      'Development map with the use of Google Maps API',
-      'Development internal platform with the use of React, Redux, and Material UI',
-      'Testing and debugging',
-    ],
-  },
-  {
-    company: 'Imaginamos',
-    link: 'https://imaginamos.com/',
-    location: 'Bogotá D C, Colombia',
-    position: 'Frontend developer',
-    start: '2021',
-    end: '2021',
-    tasks: [
-      'Development of the Imaginamos platform with the use of React, Redux, and Material UI',
-      'Work in ETB project with the use of React, Redux, and Material UI',
-    ],
-  },
-  {
-    company: 'INETUM',
-    location: 'Bogotá D C, Colombia',
-    position: 'Frontend developer',
-    start: '2021',
-    link: 'https://www.inetum.com/es',
-    end: '2021',
-    tasks: [
-      'Support in the QA area and bug review',
-      'Use of SCRUM methodology',
-      'Claro projects with the use of frameworks and libraries such as react and angular',
-      'Use of redux toolkit as aproposal which allowed a shorter development time when using this tool',
-    ],
-  },
-  {
-    company: 'S I G',
-    location: 'Bogotá D C, Colombia',
-    position: 'Software developer',
-    start: '2021',
-    end: '2021',
-    tasks: [
-      'Development of a dashboard with react admin and chartjs for managing and entering information',
-    ],
-  },
-  {
-    company: 'BOOKII',
-    location: 'Bogotá D C, Colombia',
-    position: 'Software developer',
-    start: '2019',
-    end: '2021',
-    tasks: [
-      'Productive collaborationwiththebackendteamforthecreationofthe conversationalclubs',
-      'Effective coding of conversational clubs following design guide lines and using the Redux statemanager',
-      'Creation of the Bookii page in Spanish and change of texts in cms',
-      'Generation of static posts using Gatsby and the Contentful cms for the Bookii blog',
-    ],
   },
 ]
--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -1,9 +1,11 @@
 import contentful from 'contentful'
 
 export const contentfulClient = contentful.createClient({
-  space: import.meta.env.CONTENTFUL_SPACE_ID,
-  accessToken: import.meta.env.DEV
-    ? import.meta.env.CONTENTFUL_PREVIEW_TOKEN
-    : import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
-  host: import.meta.env.DEV ? 'preview.contentful.com' : 'cdn.contentful.com',
+  space: import.meta.env.CONTENTFUL_SPACE_ID, // ID del espacio Contentful
+  accessToken: import.meta.env.DEV // Si es un entorno de desarrollo, se usa el token de previsualización
+    ? import.meta.env.CONTENTFUL_PREVIEW_TOKEN 
+    : import.meta.env.CONTENTFUL_DELIVERY_TOKEN, // Si no es desarrollo, se usa el token de entrega
+  host: import.meta.env.DEV 
+    ? 'preview.contentful.com' // Si es entorno de desarrollo, se conecta al host de preview
+    : 'cdn.contentful.com', // En producción, se conecta al host de contenido
 })
